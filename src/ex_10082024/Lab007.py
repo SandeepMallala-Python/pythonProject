4239b72c96e72e3e83c35914c4b97c3ae4243e5b
LAPTOP-OR12R69B\sande <mallala.sandeep@gmail.com>
2024-08-13 07:23:25 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/ex_10082024/Lab003.py

---
+++ src/ex_10082024/Lab007.py
@@ -0,0 +1,27 @@
+a=10
+_=20
+print(a)
+print(_)
+print(_+a)
+# 123=40  Cannot assign value to a numbers
+# print(123)
+abc123=50
+print(abc123)
+
+# 123abc=50 Cannot assign value to a variable starting with number
+# print(123abc)
+print(type(a))
+name="Sandeep Mallala"
+print(name)
+print(type(name))
+
+iSmale=True
+print(iSmale)
+print(type(iSmale))
+
+pi = 3.14
+print(type(pi))
+
+complex_number = 2 + 3j
+print(complex_number.real)
+print(complex_number.imag)
