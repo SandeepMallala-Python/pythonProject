aad5f18d5099eba0014c1e85ec476e273df1cb37
LAPTOP-OR12R69B\sande <mallala.sandeep@gmail.com>
2024-08-16 20:59:29 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/ex_10082024/Lab003.py

---
+++ src/ex_13082024/Lab012.py
@@ -0,0 +1,17 @@
+# Calculator Program - Add/Sub/Div/Mul
+
+a=int(input("Enter a value"))
+b=int(input("Enter b value"))
+sum=(a+b)
+print(sum)
+sub=(a-b)
+print(sub)
+mul=(a*b)
+print(mul)
+div=(a/b)
+print(div)
+
+print(type(sum))
+print(type(sub))
+print(type(mul))
+print(type(div))
