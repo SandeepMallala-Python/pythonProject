728ca179f93122d72e5fd9b962ad43ebe84f540e
LAPTOP-OR12R69B\sande <mallala.sandeep@gmail.com>
2024-08-16 21:40:45 +0530

    Adding Lab015

---
+++ src/ex_13082024/Lab015.py
@@ -0,0 +1,8 @@
+# Print table 10
+table = 10
+print(f"{table}*1={table}")
+print(f"{table}*2={table*2}")
+print(f"{table}*3={table*3}")
+print(f"{table}*4={table*4}")
+print(f"{table}*5={table*5}")
+print(f"{table}*6={table*6}")
