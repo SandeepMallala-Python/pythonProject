57e3d53454ea89f0da194f61a73e4691093e41a4
LAPTOP-OR12R69B\sande <mallala.sandeep@gmail.com>
2024-08-16 21:10:35 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/ex_10082024/Lab003.py

---
+++ src/ex_13082024/Lab013.py
@@ -0,0 +1,14 @@
+# - Formatting a number
+pi_value=3.14568798765
+formatted_number=f"{pi_value:.5f}"
+formatted_number1=f"{pi_value:.2f}"
+formatted_number2=f"{pi_value:.6f}"
+formatted_number3=f"{pi_value:.2f}"
+formatted_number4=f"{pi_value:.1f}"
+formatted_number5=f"{pi_value:.0f}"
+print(formatted_number)
+print(formatted_number1)
+print(formatted_number2)
+print(formatted_number3)
+print(formatted_number4)
+print(formatted_number5)
