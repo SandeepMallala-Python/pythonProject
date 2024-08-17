3ead5b6b225fb323a48bf321f74c956bf7195d0c
LAPTOP-OR12R69B\sande <mallala.sandeep@gmail.com>
2024-08-17 18:01:06 +0530

    ### Task #3 - Explain the difference between the = operator and the == operator in Python. - What does the ** operator do in Python, and how is it used? - What does the ^ operator do in Python, and in what context is it commonly used?

---
+++ src/ex_17082024/Task3_17082024.py
@@ -0,0 +1,27 @@
+### Task #3
+# Explain the difference between the = operator and the == operator in Python.
+"""
+= operator is used for assignment operator whereas == operator is used for comparing 2 values and returns true or false
+based on comparison
+"""
+a=int(input("Enter number a"))
+b=int(input("Enter number b"))
+res1=f"{a}={b}"
+print(res1)
+print(a==b)
+
+# What does the ** operator do in Python, and how is it used?
+""" ** operators are used to calculate of the power of a number """
+x=int(input("Enter number x"))
+y=int(input("Enter number y"))
+print(x**y)
+
+# What does the ^ operator do in Python, and in what context is it commonly used?
+x=int(input("Enter number x"))
+y=int(input("Enter number y"))
+print(x^y)
+
+
+
+
+
