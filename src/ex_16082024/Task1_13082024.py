22e88ad3b4c872c14f9e765d0df0b2af0087dc12
LAPTOP-OR12R69B\sande <mallala.sandeep@gmail.com>
2024-08-17 06:52:47 +0530

    Adding Lab017 - Updated Datatype

---
+++ src/ex_16082024/Task1_13082024.py
@@ -0,0 +1,13 @@
+# Printing table using userinput
+table=float(input("Enter table Number"))
+num=1
+print(f"{table}*1={table*num}")
+print(f"{table}*2={table*(num+1)}")
+print(f"{table}*3={table*(num+2)}")
+print(f"{table}*4={table*(num+3)}")
+print(f"{table}*5={table*(num+4)}")
+print(f"{table}*6={table*(num+5)}")
+print(f"{table}*7={table*(num+6)}")
+print(f"{table}*8={table*(num+7)}")
+print(f"{table}*9={table*(num+8)}")
+print(f"{table}*10={table*(num+9)}")
