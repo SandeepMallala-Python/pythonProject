cea21eda7298fecc73bb6870bdf8073974439a63
LAPTOP-OR12R69B\sande <mallala.sandeep@gmail.com>
2024-08-23 21:50:26 +0530

    Tasks

---
+++ src/ex_17082024/Task8_Square_Cube.py
@@ -0,0 +1,7 @@
+# Develop a Python script that calculates the square and cube of a given number.
+
+
+num=int(input("Enter a number : "))
+
+print("Square of a number :" ,pow(num,2))
+print("Cube of a number :",pow(num,3))
---
+++ src/exc_20082024/Lab021.py
@@ -0,0 +1,11 @@
+name=input("Enter a Name\n")
+
+print("EnteredName:"f"{name}")
+print("EnteredName:"f"{len(name)}")
+print("EnteredName:"f"{name.upper()}")
+print("EnteredName:"f"{name.lower()}")
+print("EnteredName:"f"{name.capitalize()}")
+
+
+
+
